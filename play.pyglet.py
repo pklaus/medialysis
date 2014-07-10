cf750e33bce5c650e66d35dbf2111548b6b6bb47
Philipp Klaus <philipp.klaus@gmail.com>
2014-07-10 15:00:39 +0200

    pyglet player simplified

--- play.pyglet.py
+++ play.pyglet.py
@@ -1,26 +1,16 @@
 #!/usr/bin/env python
 
 import sys
+import pyglet
 
 try:
     wavefile = sys.argv[1]
 except:
     print("specify wave file!"); sys.exit(2)
 
-import pyglet
-
 player = pyglet.media.Player()
-
-
 music = pyglet.media.load(wavefile)
-
 player.queue(music)
 player.play()
-import time
-
-while True:
-    time.sleep(2)
-    player.seek(12.0)
-
 pyglet.app.run()
 
