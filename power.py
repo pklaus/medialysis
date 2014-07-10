fc2d0917c250b1baead7ac867ce9d7311bb732cc
Philipp Klaus <philipp.klaus@gmail.com>
2014-07-10 16:30:46 +0200

    argparse CLI added

--- power.py
+++ power.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import wave
 import sys
 import struct
@@ -8,64 +9,53 @@ import time
 import pyglet
 import math
 
+parser = argparse.ArgumentParser(description='Analyze RMS values and calculate dB of sections in audio files.')
+parser.add_argument('audiofile', help='Audio file to analyse')
+parser.add_argument('--skip', metavar='SECONDS', default=0.0, type=float, help='Seconds to skip in the beginning')
+parser.add_argument('--chunksize', metavar='SAMPLES', default=2**12, type=int, help='Size of the chunks to analyse')
+parser.add_argument('--threshold-on', metavar='dB', default=-30.0, type=float, help='dB value to start marking a section as interesting.')
+parser.add_argument('--threshold-off', metavar='dB', default=-40.0, type=float, help='dB value to stop marking a section as interesting.')
+parser.add_argument('--confirm-continue', action='store_true', help='If active, you must confirm (press enter) to continue after each interesting section.')
+args = parser.parse_args()
 
-try:
-    wavefile = sys.argv[1]
-except:
-    print("specify wave file!"); sys.exit(2)
-
-try:
-    skipseconds = float(sys.argv[2])
-except:
-    skipseconds = 0.0
-
-w = wave.open(wavefile, mode='rb')
+w = wave.open(args.audiofile, mode='rb')
 player = pyglet.media.Player()
-music = pyglet.media.load(wavefile)
+music = pyglet.media.load(args.audiofile)
 player.queue(music)
 
-nframes = 2**12
+chunksize = args.chunksize
 samplewidth = w.getsampwidth()
 framerate = w.getframerate()
 nchannels = w.getnchannels()
-
-confirm_continue = True
-thresh_on_dB  = -42.
-thresh_off_dB = -52.
-#thresh_on_dB  = -39.
-#thresh_off_dB = -44.
-#thresh_on_dB  = -35.
-#thresh_off_dB = -39.
 
 def get_dB(samplevalue):
     return 20*math.log(float(samplevalue) /(2**15-1))/math.log(10)
 
 # convert skip from s to frames:
-skipframes = int(skipseconds * framerate / nframes)
+skipframes = int(args.skip * framerate / chunksize)
 print("Skipping {} frames".format(skipframes))
 
 rms = []
 on = False
 loud_sections_duration = []
 #for i in range(skipframes):
-#    w.readframes(nframes)
-w.setpos(skipframes*nframes)
-player.seek(float(skipframes * nframes) / framerate)
+#    w.readframes(chunksize)
+w.setpos(skipframes*chunksize)
+player.seek(float(skipframes * chunksize) / framerate)
 player.play()
 time.sleep(0.5)
 i = skipframes
 while True:
-    f = w.readframes(nframes)
-    if len(f) < (nframes * samplewidth): break
+    f = w.readframes(chunksize)
+    if len(f) < (chunksize * samplewidth): break
     #import pdb; pdb.set_trace()
-    d = struct.unpack("<" + str(nframes) + "h", f)
-    position = float(i * nframes) / framerate
+    d = struct.unpack("<" + str(chunksize) + "h", f)
+    position = float(i * chunksize) / framerate
     value = audioop.rms(f, samplewidth)
     dB = get_dB(value)
     #print(value)
     rms.append(value)
-    #if not on and value >= thresh_on:
-    if not on and dB >= thresh_on_dB:
+    if not on and dB >= args.threshold_on:
         on = True
         player.seek(position)
         player.play()
@@ -73,8 +63,7 @@ while True:
         sys.stdout.flush()
         print("getting loud at {:.3f} s".format(position))
         loud_rms_values = []
-    #elif on and value <= thresh_off:
-    elif on and dB <= thresh_off_dB:
+    elif on and dB <= args.threshold_off:
         on = False
         max_rms = max(loud_rms_values)
         print("Maximum value in loud phase: {} ({:.1f} dB)".format(max_rms, get_dB(max_rms)))
@@ -87,7 +76,7 @@ while True:
             if st > 0.0: time.sleep(st)
             else: break
         player.pause()
-        if confirm_continue:
+        if args.confirm_continue:
             input("Please press [Enter] to continue")
     if on: loud_rms_values.append(value)
     else:
@@ -99,8 +88,8 @@ if len(loud_sections_duration) > 0:
     print("Number of loud sections: {}".format(len(loud_sections_duration)))
     average_sections = sum(loud_sections_duration)/float(len(loud_sections_duration))
     print("Average number of pieces in loud section: {:.1f}".format(average_sections))
-    print("Average duration of loud section: {:.3f} s".format( average_sections * nframes / framerate ))
+    print("Average duration of loud section: {:.3f} s".format( average_sections * chunksize / framerate ))
 else:
     print("No loud sections found!")
-print("Analyzed {} pieces of {} samples each.".format(len(rms), nframes))
+print("Analyzed {} pieces of {} samples each.".format(len(rms), chunksize))
 
