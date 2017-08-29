1055ee87313a4f1d3cfd23d667f00ba4d93b6460
Philipp Klaus <philipp.klaus@gmail.com>
2017-08-29 03:43:26 +0200

    plot-picklefile.py now with matplotlib instead of gnuplot

--- plot-picklefile.py
+++ plot-picklefile.py
@@ -3,19 +3,42 @@
 import pickle
 import sys
 
-try:
-    picklefile = sys.argv[1]
-except:
-    print("Specify pickle file and plot like this:")
-    print("./plot-picklefile.py datafile.p | gnuplot -persistant")
-    sys.exit(2)
+import matplotlib as mpl
+mpl.rcParams['agg.path.chunksize'] = 10000
+from matplotlib import pyplot as plt
+from datetime import datetime as dt, timedelta as td
+import numpy as np
 
-values = pickle.load( open( picklefile, "rb" ) )
+def myFormatter(in_value_in_seconds, pos=None):
+    hours = int(in_value_in_seconds//(3600))
+    in_value_in_seconds -= hours * 3600
+    mins = int(in_value_in_seconds//(60))
+    in_value_in_seconds -= mins * 60
+    secs = int(in_value_in_seconds)
 
-print("# The output of this tool can directly be fed into gnuplot via the pipe '|'")
-print("# number of values: {}".format(len(values))
-print("set yrange [0:20 < * < 200]")
-print("plot '-' with filledcurves")
-for i in range(len(above_thresh)):
-    print("{:.3f} {:3d}".format(i, above_thresh[i]))
-print("e")
+    return f"{hours:02d}:{mins:02d}:{secs:02d}"
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('picklefile')
+    args = parser.parse_args()
+
+    with open(args.picklefile, "rb") as f:
+        above_thresh = pickle.load(f)
+
+    above_thresh = np.log(above_thresh)
+
+    #x_td = [td(seconds=i) for i in range(len(above_thresh))]
+    x = [i/30. for i in range(len(above_thresh))]
+
+    ax = plt.gca()
+    ax.xaxis.set_major_formatter(plt.FuncFormatter(myFormatter))
+    #ax.xaxis.set_major_formatter(myFormatter)
+    #plt.plot_date(x_td, above_thresh)
+    plt.plot(x, above_thresh)
+    #ax.set_yscale("log", nonposy='clip')
+    #plt.semilogy(x, above_thresh)
+    plt.show()
+
+if __name__ == "__main__": main()
